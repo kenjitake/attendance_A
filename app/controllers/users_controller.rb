5da3c61e732f2aae01d992f94e0818fdbe62c15d
kenji <kenji@email.com>
2023-04-20 15:15:24 +0000

    users#show追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
   
+  
+  def show
+    @user= User.find(params[:id])
+  end
+  
+  
   def new
     @user = User.new
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success]= "ユーザーが作成されました"
+      redirect_to @user
+    else
+      render:new
+    end
   end
+  
+  
+  
+    private
+    
+    def user_params
+      params.require(:user).permit(:name, :department,
+                    :employee_number, :cardID, :password, :password_confirmation)
+    end
   
 end
