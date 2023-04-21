0ea92519ff2f6cb9bec586d360f378c753342231
kenji <kenji@email.com>
2023-04-21 06:08:55 +0000

    sessionsコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   
+  before_save { self.email = email.downcase }
+  
   validates:name, presence: true
   
   validates:department, presence: true, allow_nil: true
@@ -7,6 +9,11 @@ class User < ApplicationRecord
   validates:employee_number, presence: true
   
   validates:cardID, presence: true
+  
+  VALID_EMAIL_REGIX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates:email, presence: true, length: { maximum: 50 },
+                 format: { with: VALID_EMAIL_REGIX }
   
   has_secure_password
   validates:password, presence:true, length: { minimum: 6 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
